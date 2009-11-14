083e8a169975a53b8779421560541dcad714370d
Zach Dennis <zach.dennis@gmail.com>
2009-11-14 16:31:36 -0500

    Added #flash_messages helper for layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_messages
+    returning "" do |str|
+      %w(notice failure success).each do |key|
+        str << content_tag(:p, flash[key], :class => key) if flash[key]
+      end
+    end
+  end
+  
+  
   def section(klasses)
     content_for(:section, klasses)
   end
