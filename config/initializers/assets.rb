3eb6066ef1f80eaece5423078fb445eac9ca3f04
Zach Dennis <zach.dennis@gmail.com>
2009-11-14 17:04:16 -0500

    Extracted common JS out into config/initializers, updated login styles.

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+ActionView::Helpers::AssetTagHelper::register_javascript_include_default("jquery")
+ActionView::Helpers::AssetTagHelper::register_javascript_include_default("tiny_mce/jquery.tinymce.js")
+ActionView::Helpers::AssetTagHelper::register_javascript_include_default("tiny_mce/tiny_mce_config.js")
