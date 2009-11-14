d3305bb6a87698c608b376a969a03aedd19eeab9
Zach Church <zgchurch@gmail.com>
2009-11-14 17:45:31 -0500

    Add image and video buttons to TinyMCE

--- public/javascripts/tiny_mce/tiny_mce_config.js
+++ public/javascripts/tiny_mce/tiny_mce_config.js
@@ -8,7 +8,7 @@ $().ready(function() {
 		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
 		// Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,|,cut,copy,paste,pastetext,pasteword,|,undo,redo",
+		theme_advanced_buttons1 : "bold,italic,underline,|,cut,copy,paste,pastetext,pasteword,|,undo,redo,|,anchor,image",
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
 		theme_advanced_toolbar_location : "top",
