d5e87607b2a4b1c0b8b3e85d52903a8fd480af2c
Zach Church <zgchurch@gmail.com>
2009-11-15 09:53:59 -0500

    Merge

--- features/step_definitions/link_steps.rb
+++ features/step_definitions/link_steps.rb
@@ -1,6 +1,6 @@
 Given /^a link exists for the "([^\"]*)" park called "([^\"]*)"$/ do |park_name, link_title|
   park = Park.find_by_name(park_name)
-  park.links.create!(:title => link_title)
+  Factory(:link, :title => link_title, :park => park)
 end
 
 When /^I follow "([^\"]*)" for the "([^\"]*)" link$/ do |link, link_title|
