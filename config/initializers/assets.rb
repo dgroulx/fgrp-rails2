8cf17893a484f9f3b84abbc1756eebb10797aaf2
Zach Dennis <zach.dennis@gmail.com>
2009-11-15 11:45:47 -0500

    Updated assets.rb initializer to override defaults so no prototype crap is loaded.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,1 @@
-ActionView::Helpers::AssetTagHelper::register_javascript_include_default("jquery")
-ActionView::Helpers::AssetTagHelper::register_javascript_include_default("tiny_mce/jquery.tinymce.js")
-ActionView::Helpers::AssetTagHelper::register_javascript_include_default("tiny_mce/tiny_mce_config.js")
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :defaults => ["jquery", "tiny_mce/jquery.tinymce.js", "tiny_mce/tiny_mce_config.js"]
