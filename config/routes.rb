c7333bf99ed060c140140d8ce53b895b2f186e93
Zach Dennis <zach.dennis@gmail.com>
2009-11-15 13:45:26 -0500

    Added a logout link the admin layout.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'explore'
 
   map.resource :session, :controller => 'sessions'
+  
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 end
