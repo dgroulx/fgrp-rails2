780027c82ac2a3691e9aa2400d27d164db97314a
Drew Colthorp <colthorp@atomicobject.com>
2009-11-15 12:06:52 -0500

    fixed page amenity ui

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,71 +1,99 @@
 
 $(document).ready(function() {	
-  var adminArea = $("body.parks.admin"); //refactor this out
-  var address = adminArea.find(".address:first");
+  (function() {
+    var adminArea = $("body.parks.admin"); //refactor this out
+    var address = adminArea.find(".address:first");
   
-  function logMapMessage(pMessage)
-  {
-    adminArea.find(".message:first").html(pMessage);
-  }
-  function updateLatLngInputs(pGLatLng)
-  {
-    adminArea.find(".latitude:first").val(pGLatLng.lat());
-    adminArea.find(".longitude:first").val(pGLatLng.lng());
-  }
-  function updateMap(pSelector,pAddress,pPlotmarker,pUpateLatLng)
-  {
-    var adminMap = new GMap2($(pSelector)[0]);
-    var geoCoder = new GClientGeocoder();
-    
-    if(pAddress.length==0)
+    function logMapMessage(pMessage)
     {
-      pAddress = "Grand Rapids,MI";
-      //address.val(pAddress);
+      adminArea.find(".message:first").html(pMessage);
     }
+    function updateLatLngInputs(pGLatLng)
+    {
+      adminArea.find(".latitude:first").val(pGLatLng.lat());
+      adminArea.find(".longitude:first").val(pGLatLng.lng());
+    }
+    function updateMap(pSelector,pAddress,pPlotmarker,pUpateLatLng)
+    {
+      var adminMap = new GMap2($(pSelector)[0]);
+      var geoCoder = new GClientGeocoder();
     
-    geoCoder.getLatLng(pAddress,function(pLatLng){
-      if(pLatLng===null)
+      if(pAddress.length==0)
       {
-        logMapMessage("Address not found.");
-        return;
+        pAddress = "Grand Rapids,MI";
       }
-      else
-      {
-        adminMap.setCenter(pLatLng, 10);
-        adminMap.clearOverlays();
-        if(pUpateLatLng)
-          updateLatLngInputs(pLatLng);
-        if(pPlotmarker)
+    
+      geoCoder.getLatLng(pAddress,function(pLatLng){
+        if(pLatLng===null)
         {
-          var marker = new GMarker(pLatLng);
-          adminMap.addOverlay(marker);
+          logMapMessage("Address not found.");
+          return;
         }
-      }
+        else
+        {
+          adminMap.setCenter(pLatLng, 10);
+          adminMap.clearOverlays();
+          if(pUpateLatLng)
+            updateLatLngInputs(pLatLng);
+          if(pPlotmarker)
+          {
+            var marker = new GMarker(pLatLng);
+            adminMap.addOverlay(marker);
+          }
+        }
+      });
+    }
+  
+    $('body.parks.admin .update-address:first').click(function() {
+      var geoCoder = new GClientGeocoder();
+      geoCoder.getLatLng(address.val(),function(pLatLng){
+        if(pLatLng===null)
+          logMapMessage("Address could not be found");
+        else
+        {
+          logMapMessage("Address found successfully.");
+          updateMap("#admin-map",address.val(),true,true);
+        }
+      }); 
+      return false;
     });
-  }
   
-  $('body.parks.admin .update-address:first').click(function() {
-    var geoCoder = new GClientGeocoder();
-    geoCoder.getLatLng(address.val(),function(pLatLng){
-      if(pLatLng===null)
-        logMapMessage("Address could not be found");
-      else
-      {
-        logMapMessage("Address found successfully.");
+  
+    if($("#admin-map").length>0)
+    {
+      if(adminArea.find(".latitude").val().length>0 && adminArea.find(".longitude").val().length>0)
         updateMap("#admin-map",address.val(),true,true);
-      }
-    }); 
-    return false;
-  });
-  
-  
-  if($("#admin-map").length>0)
-  {
-    if(adminArea.find(".latitude").val().length>0 && adminArea.find(".longitude").val().length>0)
-      updateMap("#admin-map",address.val(),true,true);
-    else
-      updateMap("#admin-map","Grand Rapids, MI",false,false);
-  }
+      else
+        updateMap("#admin-map","Grand Rapids, MI",false,false);
+    }
+  })();
+    
+
+
+
+  // amenities on teh parks admin
+  (function() {
+    var amenity_template = $('body.parks.admin .amenities .template').remove().html();
+    var amenity_counter = $('body.parks.admin .amenities .amenity').length;
+
+    $('body.parks.admin .add_amenity').each(function() {
+      $(this).click(function() {
+        var this_template_content = amenity_template.replace(/NEW_RECORD/g, amenity_counter);
+        $('body.parks.admin .amenities').append(this_template_content);
+        amenity_counter++;
+        return false;
+      });
+    });
+
+    $('body.parks.admin .amenity a.delete').
+      live('click', function() {
+        console.log(this);
+        $(this).parents(".amenity").
+          hide().
+          find('input[name*=_delete]').
+            attr('value', 1);
+        return false;})
+  })();
   
 });
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
-  var amenity_template = $('body.parks.admin .amenities .template').remove().html();
-  var amenity_counter = $('body.parks.admin .amenities .amenity').length;
-  
-  $('body.parks.admin .add_amenity').each(function() {
-    $(this).click(function() {
-      var this_template_content = amenity_template.replace(/NEW_RECORD/g, amenity_counter);
-      $('body.parks.admin .amenities').append(this_template_content);
-      amenity_counter++;
-      return false;
-    });
-  });
-  
-  $('body.parks.admin .amenity .delete').each(function() {
-    $(this).click(function() {
-      $(this).parent().siblings('input[type=hidden]:first').attr('value', 1);
-      $(this).parent().parent().hide();
-      return false;
-    });
-  });
-  
-  
   $('.flickr-pool-slideshow').each(function() {
     var element = $(this);
     var key = "40343c1f16327beb5c9adfd73caec372";
